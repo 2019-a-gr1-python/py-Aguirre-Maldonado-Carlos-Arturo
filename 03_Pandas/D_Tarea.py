644b51711296801be1154a014ddc496b818b7f68
Carlos Aguirre Maldonado <caguirremal11@gmail.com>
2019-06-19 00:20:08 -0500

    Tarea Formatos Excel

--- 03_Pandas/D_Tarea.py
+++ 03_Pandas/D_Tarea.py
@@ -1,91 +1,26 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 12 07:22:18 2019
+Created on Tue Jun 18 20:53:30 2019
 
-@author: carlosaguirre
+@author: CarlosHP
 """
+
 import pandas as pd
 import numpy as np
 import os
 import sqlite3
 
-#path_guardado = '/Users/carlosaguirre/Documents/GitHub/py-Aguirre-Maldonado-Carlos-Arturo/03_Pandas/data/csv/artwork_data.pickle'
-
 path_guardado = 'C:\\Users\\Carlos\\Documents\\GitHub\\py-Aguirre-Maldonado-Carlos-Arturo\\03_Pandas\\data\\csv\\artwork_data.pickle'
 
-
-df_completo_pickel = pd.read_pickle(path_guardado)
 df_completo_pickle = pd.read_pickle(path_guardado)
 
-df = df_completo_pickel.iloc[49980:50019,:].copy()
-
-#   Tipos de archivos
-#   £
-#   - JSON
-#   - SQL
-#   - EXCEL
-
-##################################EXCEL#######################################
+df = df_completo_pickle.iloc[49980:50019,:].copy()
 
 df.to_excel('ejemplo_basico.xlsx')
 
-df.to_excel('ejemplo_basico_sin_indices.xlsx', index=False)
+writer = pd.ExcelWriter('formatoDeber.xlsx', engine = 'xlsxwriter')
 
-columnas = ['artist','title','year']
-
-df.to_excel('columnas.xlsx', columns = columnas)
-
-# Múltiples hojas de trabajo (worksheet)
-
-writer = pd.ExcelWriter('multiples_worksheet.xlsx',
-                        engine = 'xlsxwriter')
-
-df.to_excel(writer, sheet_name = 'Preview')
-
-df.to_excel(writer, sheet_name = 'Preview Dos', index = False)
-
-df.to_excel(writer, sheet_name = 'Preview', columns = columnas)
-
-writer.save()
-
-artistas_contados = df_completo_pickel['artist'].value_counts()
-
-writer = pd.ExcelWriter('colores.xlsx', engine = 'xlsxwriter')
-
-artistas_contados.to_excel(writer, sheet_name = 'Artistas contados')
-
-hoja_artistas = writer.sheets['Artistas contados']
-
-rango_celdas = 'B2:B{}'.format(len(artistas_contados.index)+1)
-
-formato = {
-        'type':'2_color_scale',
-        'min_value':'10',
-        'min_type':'percentile',
-        'max_value':'99',
-        'max_type':'percentile'       
-        }
-hoja_artistas.conditional_format(rango_celdas, formato)
-
-writer.save()
-
-
-#################################### SQL ####################################
-
-with sqlite3.connect('bdd_python.db') as conexion:
-    df.to_sql('Alguien', conexion)
-
-###with sqlite3.connect('mysql))') as conexion:
-###    ds.to_sql('Alguien', conexion)
-
-################################# JSON #######################################
-    
-df.to_json('artistas.json')
-#######
-
-
-#########################TAREA: Aplicar los demás formatos ##################
+############################################################################
 
 artistas_contados = df_completo_pickle['artist'].value_counts()
 
