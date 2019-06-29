20135fde2e71a3b78823bcb4475d1edc98883bfc
Carlos Aguirre Maldonado <caguirremal11@gmail.com>
2019-06-29 10:59:22 -0500

    ejercicios arañas
    - Reglas araña obtención de entidades
    - Araña obtención de libros

---
+++ 05_Scrapy/02_Crawling/python_02_followlink/python_02/python_02/spiders/arania_onu.py
@@ -0,0 +1,51 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class AraniaCrawlOnu(CrawlSpider):
+    name = 'crawl_onu' # Heredado (conservar el nombre)
+    allowed_domains = [
+        'un.org'
+    ]
+    start_urls = [ # Heredado (conservar el nombre)
+        'https://www.un.org/en/sections/about-un/funds-programmes-specialized-agencies-and-others/index.html'
+    ]
+    regla_uno = ( # Heredado (conservar el nombre)
+        Rule(LinkExtractor(), callback='parse_page'),
+    )
+
+    url_segmento_permitido = (
+        'funds-programmes-specialized-agencies-and-others'
+    )
+
+    url_segmento_restringido = (
+        'ar/sections',
+        'zh/sections',
+        'ru/sections'
+    )
+
+    regla_dos = ( # Heredado (conservar el nombre)
+        Rule(LinkExtractor(
+            allow_domains=allowed_domains,
+            allow=url_segmento_permitido
+        ), callback='parse_page')
+        ,
+    )
+
+    regla_tres = ( # Heredado (conservar el nombre)
+        Rule(LinkExtractor(
+            allow_domains=allowed_domains,
+            allow=url_segmento_permitido,
+            deny=url_segmento_restringido
+        ), callback='parse_page')
+        ,
+    )
+
+    rules = regla_tres
+
+    def parse_page(self, response):
+        lista_programas = response.css('div.field-items > div.field-item > h4::text').extract()
+
+        for agencia in lista_programas:
+            with open('onu_agencias.txt','a+') as archivo:
+                archivo.write(agencia + '\n')
